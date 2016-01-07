632b3e72344a36fd31c09cc6720613f7df3d3ee4
Jyri Andrejev <juri@salemove.com>
2016-01-07 17:30:20 +0200

    Add deliver_with_response function

--- spec/mercury/core_spec.clj
+++ spec/mercury/core_spec.clj
@@ -5,18 +5,28 @@
 
 (def test-queue (atom {}))
 
+(def response-message "OK")
+
 (defn test-adapter [queue]
   {:deliver (fn [client service payload]
               (reset! queue {service payload})
-              nil)})
+              nil)
+   :deliver-with-response (fn [client service payload]
+                            (reset! queue {service payload})
+                            response-message)})
 
 
 (def test-client {:adapter (test-adapter test-queue)})
 
-(describe "Mercury"
+(def message "random message")
+(def service :test_service)
+
+(describe "Mercury interface"
   (it "delivers a message"
-      (let [message "random message"
-            service :test_service
-            resp (mercury/deliver test-client service msg)
-            response-message (service @test-queue)]
-      (should= message response-message))))
+    (let [resp (mercury/deliver test-client service message)
+          response-message (service @test-queue)]
+      (should= message response-message)))
+
+  (it "delivers a message and waits for response"
+    (let [response (mercury/deliver-with-response test-client service message)]
+      (should= response response-message))))
--- src/mercury/core.clj
+++ src/mercury/core.clj
@@ -4,3 +4,8 @@
   (let [adapter (:adapter client)
         func    (:deliver adapter)]
     (func client service-name payload)))
+
+(defn deliver-with-response [client service-name payload]
+  (let [adapter (:adapter client)
+        func    (:deliver-with-response adapter)]
+    (func client service-name payload)))
